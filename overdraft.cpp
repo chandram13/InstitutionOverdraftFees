e94b3670cc9297335bbbd7d41115a231025b63be
Marvish Subhas Chandra <mschandr@uci.edu>
2022-10-23 10:13:36 -0700

    Add files via upload

---
+++ overdraft.cpp
@@ -0,0 +1,449 @@
+// Marvish Chandra
+
+#include <iostream>
+
+class Alliant{
+    public:
+    int fee1;
+    int max;
+    int totalfee;
+};
+  public static void alliantRates(String[] args){
+        Alliant alliantbank;
+        alliantbank.fee1 = 0;
+        alliantbank.max = 0;
+        alliantbank.totalfee = 0;
+    };
+class Ally{
+    public:
+    int fee1;
+    int max;
+    int totalfee;
+};
+    public static void allyRates(String[] args){
+        Ally allybank;
+        allybank.fee1 = 0;
+        allybank.max = 0;
+        allybank.totalfee = 0;
+    };
+class AmericanExpressNationalBank{
+    public:
+    int fee1;
+    int max;
+    int totalfee;
+};
+public static void americanexpressnationalbank(String[] args){
+    AmericanExpressNational americanexpressnationalbank;
+    americanexpressnationalbank.fee1 = 0;
+    americanexpressnationalbank.max = 0;
+    americanexpressnationalbank.totalfee = 0;
+};
+class AssociatedBank{
+    public:
+    int fee1;
+    int max;
+    int totalfee;
+};
+public static void associatedbank(String [] args){
+    Associated associatedbank;
+    associatedbank.fee1 = 036;
+    associatedbank.max = 4;
+    associatedbank.totalfee = 144;
+};
+
+class BankofAmerica{
+    public:
+    int fee1;
+    int max;
+    int totalfee;
+};
+
+public static void bankofAmerica(String[] args){
+    BankofAmerica bankofAmericabank;
+    bankofAmericabank.fee1 = 35;
+    bankofAmericabank.max = 4;
+    bankofAmericabank.totalfee = 40;
+};
+
+class BmoHarrisBank{
+    public:
+    int fee1;
+    int max;
+    int totalfee;
+};
+
+public static void bmoharrisbank(String [] args){
+    BMOHarrisBank bmoharrisbankobj;
+    bmoharrisbankobj.fee1 = 36;
+    bmoharrisbankobj.max = 3;
+    bmoharrisbankobj.totalfee = 10;
+};
+
+class BoeingEmployeesCreditUnion{
+    public:
+    int fee1;
+    int max;
+    int totalfee;
+};
+public static void boeingemployeescreditunion(String [] args){
+    BoeingEmployeesCreditUnion boeingemployeescreditunionobj;
+    boeingemployeescreditunionobj.fee1 = 25;
+    boeingemployeescreditunionobj.max = 5;
+    boeingemployeescreditunionobj.totalfee = 0;
+};
+ 
+class CapitalOne360{
+    public:
+    int fee1;
+    int max;
+    int totalfee;
+};
+
+public static void capitalone360(String [] args){
+    CapitalOne360 capitalone360bank;
+    capitalone360bank.fee1 = 0;
+    capitalone360bank.max = 0;
+    capitalone360bank.totalfee = 0;
+};
+
+class CharlesSchwab{
+    public:
+    int fee1;
+    int max;
+    int totalfee;
+};
+
+public static void charlesschwab(String [] args){
+    CharlesSchwab charlesschwabbank;
+    charlesschwabbank.fee1 = 25;
+    charlesschwabbank.max = 4;
+    charlesschwabbank.totalfee = 0;
+};
+
+class Chase{
+    public:
+    int fee1;
+    int max;
+    int totalfee;
+};
+
+public static void chasebank(String [] args){
+    Chase chasebank;
+    chasebank.fee1 = 34;
+    chasebank.max = 3;
+    chasebank.totalfee = 0;
+};
+
+class Chime{
+    public:
+    int fee1;
+    int max;
+    int totalfee;
+};
+public static void chimeBank(String [] args){
+    Chime chimebank;
+    chimebank.fee1 = 0;
+    chimebank.max = 0;
+    chimebank.totalfee = 0;
+};
+
+class Citizens{
+    public:
+    int fee1;
+    int max;
+    int totalfee;
+};
+public static void citizensBank(String [] args){
+    Citizens citizenbank;
+    citizenbank.fee1 = 37;
+    citizenbank.max = 5;
+    citizenbank.totalfee = 12;
+};
+
+class Comerica{
+    public:
+    int fee1;
+    int max;
+    int totalfee;
+};
+public static void comericaBank(String [] args){
+    Comerica comericabank;
+    comericabank.fee1 = 38;
+    comericabank.max = 5;
+    comericabank.totalfee = 0;
+};
+
+class ConnexusCreditUnion{
+    public:
+    int fee1;
+    int max;
+    int totalfee;
+};
+
+public static void connexuscreditunion(String [] args){
+    ConnexusCreditUnion connexuscreditunionobj;
+    connexuscreditunionobj.fee1 = 4;
+    connexuscreditunionobj.max = 2;
+    connexuscreditunionobj.totalfee = 0;
+};
+
+class ConsumersCreditUnion{
+    public:
+    int fee1;
+    int max;
+    int totalfee;
+};
+
+public static void consumerscreditunion(String [] args){
+    ConsumersCreditUnion consumerscreditunionobj;
+    consumerscreditunionobj.fee1 = 30;
+    consumerscreditunionobj.max = 4;
+    consumerscreditunionobj.totalfee = 10;
+};
+
+class DiscoverBank{
+    public:
+    int fee1;
+    int max;
+    int totalfee;
+};
+
+public static void discoverbank(String [] args){
+    DiscoverBank discoverbankobj;
+    discoverbankobj.fee1 = 0;
+    discoverbankobj.max = 0;
+    discoverbankobj.totalfee = 0;
+};
+
+class FifthThirdBank{
+    public:
+    int fee1;
+    int max;
+    int totalfee;
+};
+
+public static void fifththirdbank(String [] args){
+    FifthThirdBank fifththirdbankobj;
+    fifththirdbankobj.fee1 = 37;
+    fifththirdbankobj.max = 3;
+    fifththirdbankobj.totalfee = 0;
+};
+
+class FirstRepublicBank{
+    public:
+    int fee1;
+    int max;
+    int totalfee;
+};
+public static void firstrepublicbank(String [] args){
+    FirstRepublicBank firstrepublicbankobj;
+    firstrepublicbankobj.fee1 = 30;
+    firstrepublicbankobj.max = 4;
+    firstrepublicbankobj.totalfee = 0;
+};
+
+class HSBC{
+    public:
+    int fee1;
+    int max;
+    int totalfee;
+};
+
+public static void hsbcbank(String [] args){
+    HSBC hsbcbank;
+    hsbcbank.fee1 = 0;
+    hsbcbank.max = 0;
+    hsbcbank.totalfee = 0;
+};
+
+class HuntingtonBank{
+    public:
+    int fee1;
+    int max;
+    int totalfee;
+};
+
+public static void huntingtonbank(String [] args){
+    HuntingtonBank huntingtonbankobj;
+    huntingtonbankobj.fee1 = 36;
+    huntingtonbankobj.max = 3;
+    huntingtonbankobj.totalfee = 108;
+};
+
+class KeyBank{
+    public:
+    int fee1;
+    int max;
+    int totalfee;
+};
+
+public static void keybank(String [] args){
+    KeyBank keybankobj;
+    keybankobj.fee1 = 38.50;
+    keybankobj.max = 5;
+    keybankobj.totalfee = 0;
+};
+
+class M&TBank{
+    public:
+    int fee1;
+    int max;
+    int totalfee;
+};
+
+public static void m&tbank(String [] args){
+    M&TBank m&tbankobj;
+    m&tbankobj.fee1 = 36;
+    m&tbankobj.max = 5;
+    m&tbankobj.totalfee = 0;
+};
+
+class NavyFederalCreditUnion{
+    public:
+    int fee1;
+    int max;
+    int totalfee;
+};
+public static void navyfederalcreditunion(String [] args){
+    NavyFederalCreditUnion navyfederalcreditunionobj;
+    navyfederalcreditunionobj.fee1 = 20;
+    navyfederalcreditunionobj.max = 1;
+    navyfederalcreditunionobj.totalfee = 0;
+};
+
+class PNCBank{
+    public:
+    int fee1;
+    int max;
+    int totalfee;
+};
+public static void pncbank(String [] args){
+    PNCBank pncbankobj;
+    pncbankobj.fee1 = 36;
+    pncbankobj.max = 4;
+    pncbankobj.totalfee = 0;
+};
+
+class RegionsBank{
+    public:
+    int fee1;
+    int max;
+    int totalfee;
+};
+public static void regionsbank(String [] args){
+    regionsbank.fee1 = 36;
+    regionsbank.max = 5;
+    regionsbank.totalfee = 0;
+};
+
+class SantanderBank{
+    public:
+    int fee1;
+    int max;
+    int totalfee;
+};
+
+public static void santanderbank(String [] args){
+    SantanderBank santanderbankobj;
+    santanderbankobj.fee1 = 35;
+    santanderbankobj.max = 3;
+    santanderbankobj.totalfee = 0;
+};
+ 
+class TDBank{
+    public:
+    int fee1;
+    int max;
+    int totalfee;
+};
+
+public static void tdbank(String [] args){
+    TDBank tdbankobj;
+    tdbankobj.fee1 = 35;
+    tdbankobj.max = 3;
+    tdbankobj.totalfee = 3;
+};
+
+class TIAABank{
+    public:
+    int fee1;
+    int max;
+    int totalfee;
+};
+
+public static void tiaabank(String [] args){
+    tiaabank.fee1 = 0;
+    tiaabank.max = 0;
+    tiaabank.totalfee = 0;
+};
+
+class TruistBank{
+    public:
+    int fee1;
+    int max;
+    int totalfee;
+};
+
+public static void truistbank(String [] args){
+    TruistBank truistbankobj;
+    truistbankobj.fee1 = 36;
+    truistbankobj.max = 6;
+    truistbankobj.totalfee = 12.50;
+};
+
+class UnionBank{
+    public:
+    int fee1;
+    int max;
+    int totalfee;
+};
+
+public static void unionbank(String [] args){
+    UnionBank unionbankobj;
+    unionbankobj.fee1 = 33;
+    unionbankobj.max = 5;
+    unionbankobj.totalfee = 0;
+};
+
+class USAA{
+    public:
+    int fee1;
+    int max;
+    int totalfee;
+};
+
+public static bank usaabank(String [] args){
+    USAA usaabank;
+    usaabank.fee1 = 0;
+    usaabank.max = 0;
+    usaabank.totalfee = 0;
+}
+
+class USBank{
+    public:
+    int fee1;
+    int max;
+    int totalfee;
+};
+
+public static void usbank(String [] args){
+    USBank usbankobj;
+    usbankobj.fee1 = 36;
+    usbankobj.max = 4;
+    usbank.totalfee = 0;
+};
+
+class WellsFargo{
+    public:
+    int fee1;
+    int max;
+    int totalfee;
+};
+
+public static void wellsfargo(String [] args){
+    WellsFargo wellsfargobank;
+    wellsfargobank.fee1 = 35;
+    wellsfargobank.max = 4;
+    wellsfargobank.totalfee = 0;
+};
+  
